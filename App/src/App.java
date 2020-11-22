4172eb70d01d75733b76fb56ed263c96a05d318a
Joseppie <74397118+JLudwigM@users.noreply.github.com>
2020-11-21 22:33:12 -0800

    Add files via upload
    These files are for educational review

---
+++ App/src/App.java
@@ -0,0 +1,83 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub// 
+		
+		//direction three of coding steps//
+		
+		double itemprice = 1.99;
+		
+		double boarshead = 8.99;
+				
+		int moneyinwallet = 10;
+		
+		int creditcard = 100;
+		
+		float friends = 45;
+		
+		int colleagues = 2;
+		
+		int age = 25;
+		
+		int agelastyear = 24;
+		
+		String firstname = "Joseph";
+		
+		String nickname = "Joe";
+						
+		String lastname = "Ludwig";
+		
+		String misclastname = "Louis";
+		
+		char middleinitial = 'M';
+		
+		char fakenamemiddle ='K';
+
+	// system run
+		
+		System.out.println("The cheese sticks are " + itemprice + ",");
+		
+		System.out.println("then the cheese is " + boarshead + ".");
+		
+		System.out.println("I have " + moneyinwallet + " " + "dollars" + ".");
+		
+		System.out.println("If that does not work I have" + " " + creditcard + " " + "dollars on my creditcard" + ".");
+		
+		System.out.println("If anything I can ask my" + " " + friends + " " + "friends for help" + ".");
+		
+		System.out.println("and if they fail me my" + " " + colleagues + " " + "colleagues would be more than happy to help me out" + ".");
+					
+		System.out.println("I would think that at the age of" + " " + age + " " + "I would have my life together" + ".");
+		
+		System.out.println("no matter" + ", " + "I have come a long way since" + " " + agelastyear);
+		
+		System.out.println("My name is" + " " + firstname + ".");
+		
+		System.out.println("My friends call me" + " " + nickname + ".");
+				
+		System.out.println("My last name is" + " " + lastname + ".");
+		
+		System.out.println("I often think that my name would be" + " " + misclastname + " " + "if not Ludwig" + ".");
+		
+		System.out.println("This is my middle initial" + " " + middleinitial + ".");
+		
+		System.out.println("My mother would have given me the middle initial of" + " " + fakenamemiddle + ".");
+		
+		double newamount = moneyinwallet - itemprice;
+		
+		System.out.println("After going to buy the cheese sticks I have " + "$" + newamount + " " + "left" + ".");
+		
+// part 4
+		
+		float friendseachYear = friends / age;
+		
+		System.out.println("every year I make" + " " + friendseachYear + " " + "new friends" + ".");
+		
+		String fullName = firstname + " " + middleinitial + " " + lastname;
+		
+		System.out.println("My name is" + " " + fullName + " " + "and I am new to software development" + ".");
+		
+	}
+
+}
